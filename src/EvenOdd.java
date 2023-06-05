7ab7ee8302e0c76b33af04c468c4ce90f89a062a
WilliamOkine <okinewn@mail.uc.edu>
2023-06-05 13:12:00 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 17; // Hard-coded input
+
+        if (number % 2 == 0) { // using module to check to see if 17 divided by 2 provides a remainder of 0
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
+
+
